de3b6e406d16600223e6678d94ca68446461e1f9
Alex Magee <86931568+xmagee@users.noreply.github.com>
2021-07-18 00:00:43 -0400

    Fix var assign order on dice roll command, it now sets to max_dice_allowed when requested number is greater

--- commands/game.js
+++ commands/game.js
@@ -2,13 +2,12 @@
 module.exports = {
 
     roll: function(message, message_content) {
-        var dice_to_toss = message_content[1] !== undefined ? parseInt(message_content[1]) : 1,
+        var max_dice_allowed = 12, // don't set too high, else the .send() will be too big, setting to 180 will likely not work :)
+            dice_user_input = message_content[1] !== undefined ? parseInt(message_content[1]) : 1,
+            dice_to_toss = dice_user_input > max_dice_allowed ? max_dice_allowed : dice_user_input,
+            roll_results = new Array(dice_to_toss)
             dice_tossed = 0, 
-            roll_results = new Array(dice_to_toss), 
-            results_total = 0, 
-            max_dice_allowed = 12 // don't set too high, else the .send() will be too big, setting to 180 will likely not work :)
-
-        if (dice_to_toss > max_dice_allowed) { dice_to_toss = max_dice_allowed }
+            results_total = 0
 
         while (dice_tossed < dice_to_toss) {
             var dice_roll = Math.floor(Math.random() * (6 - 1) + 1)
